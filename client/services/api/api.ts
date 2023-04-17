83f0720275a44625f1894a5f01237a35733e6f98
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-04-17 17:17:36 -0300

    refactor: axios config

---
+++ client/services/api/api.ts
@@ -0,0 +1,5 @@
+import { getAPIClient } from "./clientApi";
+
+export const api = getAPIClient();
+
+// use this in browser
---
+++ client/services/api/clientApi.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { parseCookies } from "nookies";
+import { API_BASE_URL } from "../constants";
+
+export function getAPIClient(ctx?: any) {
+  const { "next.token": token } = parseCookies(ctx);
+
+  const api = axios.create({
+    baseURL: API_BASE_URL,
+  });
+
+  if (token) {
+    api.defaults.headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  return api;
+}
+
+// use this in server
