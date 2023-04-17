7383e47a6151a0322bf13dca42a8ccf00c3461d7
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-04-17 19:01:38 -0300

    fix:  secretOrPrivateKey error with dotenv

--- api/src/modules/auth/auth.module.ts
+++ api/src/modules/auth/auth.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from '../users/users.module';
+import { Module, Global } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt/dist';
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthStudentService } from './auth-student/auth-student.service';
 import { LocalStudentStrategy } from './strategies/student.strategy';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
+@Global()
 @Module({
   imports: [
     UsersModule,
