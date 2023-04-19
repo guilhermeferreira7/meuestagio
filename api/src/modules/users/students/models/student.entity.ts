b9b72280a899812973f6c32ab8ef850d27f8ec26
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-04-19 16:24:05 -0300

    feat: changed fields required to create user

--- api/src/modules/users/students/models/student.entity.ts
+++ api/src/modules/users/students/models/student.entity.ts
@@ -6,27 +6,19 @@ import { Course } from '../../../courses/models/course.entity';
 
 @Entity()
 export class Student extends User {
-  @ManyToOne(() => Institution)
-  institution: Institution;
-
-  @Column()
-  institutionId: number;
-
-  @Column()
-  institutionName: string;
-
-  @ManyToOne(() => Course)
-  course: Course;
-
-  @Column()
-  courseId: number;
-
-  @Column()
-  courseName: string;
-
+  // @ManyToOne(() => Institution)
+  // institution: Institution;
+  // @Column()
+  // institutionId: number;
+  // @Column()
+  // institutionName: string;
+  // @ManyToOne(() => Course)
+  // course: Course;
+  // @Column()
+  // courseId: number;
+  // @Column()
+  // courseName: string;
   // phone
-
   // verificar telefone email e usuario pelo prof
-
   // periodo tabela curriculo
 }
