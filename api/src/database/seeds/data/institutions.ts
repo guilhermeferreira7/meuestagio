a62528df4e2e48e850dd52a723e7d6cc0a678a65
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-05-02 21:55:25 -0300

    feat: added new seeders

---
+++ api/src/database/seeds/data/institutions.ts
@@ -0,0 +1,11 @@
+export const institutions = [
+  {
+    name: 'Universidade Tecnológica Federal do Paraná - Campus Guarapuava',
+  },
+  {
+    name: 'Universidade Estadual do Centro-Oeste',
+  },
+  {
+    name: 'Universidade Estadual de Ponta Grossa',
+  },
+];
