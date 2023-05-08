bb6feb3ac44fe4afc8665f2d72edc97faa31cdc1
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-05-08 13:49:51 -0300

    feat: add types

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -3,32 +3,23 @@ import { destroyCookie, parseCookies, setCookie } from "nookies";
 import { useRouter } from "next/router";
 
 import { api } from "@/services/api/api";
+import { UserAuth } from "../utils/types/user-auth";
 
 interface Props {
   children?: ReactNode;
 }
 
-type User = {
-  name: string;
-  email: string;
-  role: string;
-};
-
 type AuthContextType = {
   isAuthenticated: boolean;
-  user: User | null;
-  signIn: (
-    email: string,
-    password: string,
-    userType: string
-  ) => Promise<User | undefined>;
+  user: UserAuth | null;
+  signIn: (email: string, password: string, userType: string) => Promise<void>;
   signOut: () => void;
 };
 
 export const AuthContext = createContext({} as AuthContextType);
 
 export function AuthProvider({ children }: Props) {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<UserAuth | null>(null);
   const router = useRouter();
   const isAuthenticated = !!user;
 
@@ -45,13 +36,13 @@ export function AuthProvider({ children }: Props) {
   async function signIn(
     email: string,
     password: string,
-    userType: string
+    role: string
   ): Promise<any> {
-    const path = `/auth/login/${userType}`;
+    const path = `/auth/login/${role}`;
 
     const { user, access_token } = (await api.post(path, { email, password }))
       .data as { user: any; access_token: any };
-    setUser({ ...user, role: userType });
+    setUser({ ...user, role });
 
     setCookie(undefined, "next.token", access_token, {
       maxAge: 60 * 60 * 24 * 7, // 7 days
@@ -62,15 +53,14 @@ export function AuthProvider({ children }: Props) {
       path: "/",
     });
 
-    router.push(`/${userType}/dashboard`);
-    return user;
+    router.push(`/${role}/dashboard`);
   }
 
   function signOut() {
     destroyCookie(null, "next.token", { path: "/" });
     destroyCookie(null, "next.user", { path: "/" });
     setUser(null);
-    router.push("/");
+    router.push("/login");
   }
 
   return (
