267869b7ed097eb9364094a6b42b8c539f11bf27
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-05-08 13:46:34 -0300

    refactor: modified layout

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -1,13 +1,11 @@
-import Link from "next/link";
 import React, { ReactNode, useContext, useEffect } from "react";
 import { AuthContext } from "@/contexts/AuthContext";
 import StudentMenu from "./menus/student-menu";
 import DefaultMenu from "./menus/default-menu";
-import { themeChange } from "theme-change";
-import { LogOut } from "lucide-react";
-import LayoutHeader from "./header";
-import LayoutFooter from "./footer";
-import LayoutNavbar from "./navbar";
+import Footer from "./footer";
+import { Role } from "../../utils/types/user-auth";
+import Header from "./header";
+import CompanyMenu from "./menus/company-menu";
 
 interface PageLayoutProps {
   children: ReactNode;
@@ -15,26 +13,37 @@ interface PageLayoutProps {
 
 export default function PageLayout({ children }: PageLayoutProps) {
   const { user } = useContext(AuthContext);
-  useEffect(() => {
-    themeChange(false);
-  }, []);
-
   let menuItems = DefaultMenu();
-  if (user?.role === "student") {
-    menuItems = StudentMenu();
-  } else {
-    menuItems = DefaultMenu();
+
+  switch (user?.role) {
+    case Role.Student:
+      menuItems = StudentMenu();
+      break;
+    case Role.Company:
+      menuItems = CompanyMenu();
+      break;
+    case Role.Professor:
+      // menuItems = DefaultMenu();
+      break;
+    case Role.Admin:
+      // menuItems = DefaultMenu();
+      break;
+    default:
+      menuItems = DefaultMenu();
+      break;
   }
 
   return (
-    <div className="drawer drawer-mobile">
-      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col ">
-        <LayoutHeader />
-        <main className="flex h-full justify-center">{children}</main>
-        <LayoutFooter />
-      </div>
-      <LayoutNavbar menuItems={menuItems} />
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-base-100 flex flex-row justify-center drop-shadow-lg">
+        <Header menuItems={menuItems} />
+      </header>
+      <main className="text-base-content flex flex-1 justify-center overflow-auto">
+        {children}
+      </main>
+      <footer className="p-4 justify-center mt-auto border-t-2 border-opacity-10">
+        <Footer />
+      </footer>
     </div>
   );
 }
--- client/src/components/Layout/menus/company-menu.tsx
+++ client/src/components/Layout/menus/company-menu.tsx
@@ -1,16 +1,21 @@
 import React, { useContext } from "react";
 import Link from "next/link";
 import { HomeOutlined, UserOutlined, SearchOutlined } from "@ant-design/icons";
+import { PlusCircle } from "lucide-react";
 
-export default function StudentMenu() {
+export default function CompanyMenu() {
   const menuItems = [
     <Link className="w-full" href="/">
       <HomeOutlined />
       Inicio
     </Link>,
-    <Link className="w-full" href="/student/profile">
+    <Link className="w-full" href="/company/profile">
       <UserOutlined />
       Perfil
+    </Link>,
+    <Link className="w-full" href="/company/create-vacancy">
+      <PlusCircle />
+      Cadastrar vaga
     </Link>,
   ];
   return menuItems;
