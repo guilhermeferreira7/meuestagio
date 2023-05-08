94140c347318acb6f39d7de0fc9e37b6ed8fb79d
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-05-08 13:45:40 -0300

    feat: add theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,12 +11,28 @@ module.exports = {
   plugins: [require("daisyui")],
   daisyui: {
     styled: true,
-    themes: ["cupcake", "dark", "aqua"],
+    themes: [
+      {
+        mytheme: {
+          primary: "#30A7EC",
+          "primary-focus": "#2b96d4",
+          "primary-content": "#fcfcfc",
+          "base-100": "#edf6fd",
+          "base-200": "#c9e3f8",
+          "base-300": "#c0dff7",
+          "base-content": "#18191b",
+          info: "#1A5FB5",
+          error: "#dc3545",
+          warning: "#ffc107",
+          success: "#28a745",
+        },
+      },
+    ],
     base: true,
     utils: true,
     logs: true,
     rtl: false,
     prefix: "",
-    darkTheme: "dark",
+    darkTheme: "mytheme",
   },
 };
