4204fd2aeca1dd644df5699a447c9ebb4cdbbfb0
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-05-08 13:44:09 -0300

    feat: add form components

---
+++ client/src/components/Form/InputText.tsx
@@ -0,0 +1,23 @@
+import { InputHTMLAttributes } from "react";
+import { useFormContext } from "react-hook-form";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+}
+
+/**
+ * this can be used with input type text, password, email, etc
+ */
+export function InputText(props: InputProps): JSX.Element {
+  const { register } = useFormContext();
+
+  return (
+    <input
+      id={props.name}
+      type="text"
+      className="bg-white rounded input input-primary"
+      {...register(props.name)}
+      {...props}
+    />
+  );
+}
