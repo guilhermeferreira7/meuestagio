a496b9f143c0480c4bd82f815b12c8c1daa2ee5a
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-05-08 15:22:19 -0300

    feat: add companies service

---
+++ api/src/modules/users/companies/services/companies.service.ts
@@ -0,0 +1,33 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Company } from '../entities/company.entity';
+import { Repository } from 'typeorm';
+import { cp } from 'fs';
+import { CreateCompanyDto } from '../dtos/create-company.dto';
+import bcryptService from '../../../../utils/bcriptUtils';
+
+@Injectable()
+export class CompaniesService {
+  constructor(
+    @InjectRepository(Company)
+    private readonly companiesRepository: Repository<Company>,
+  ) {}
+
+  async create(company: CreateCompanyDto) {
+    const emailUsed = await this.findByEmail(company.email);
+    if (emailUsed) {
+      throw new ConflictException('Email já cadastrado!');
+    }
+
+    const password = await bcryptService.hash(company.password);
+    const newCompany = this.companiesRepository.create({
+      ...company,
+      password,
+    });
+    return await this.companiesRepository.save(newCompany);
+  }
+
+  async findByEmail(email: string): Promise<Company> {
+    return await this.companiesRepository.findOneBy({ email });
+  }
+}
