a08c213c8297f1b525036c68d0a0af79605d3cf8
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-06-20 15:10:56 -0300

    refactor: change layout

--- client/src/components/Layout/menus/admin-menu.tsx
+++ client/src/components/Layout/menus/admin-menu.tsx
@@ -4,13 +4,21 @@ import { Home, Plus } from "lucide-react";
 
 export default function AdminMenu() {
   const menuItems = [
-    <Link className="w-full" href="/">
-      <Home />
-      Início
+    <Link className="w-full flex" href="/">
+      <Home className="w-1/6" />
+      <span className="w-5/6">Início</span>
     </Link>,
-    <Link className="w-full" href="/admin/register">
-      <Plus />
-      Cadastrar
+    <Link className="w-full flex" href="/admin/register/cities">
+      <Plus className="w-1/6" />
+      <span className="w-5/6">Cadastrar cidades</span>
+    </Link>,
+    <Link className="w-full flex" href="/admin/register/areas">
+      <Plus className="w-1/6" />
+      <span className="w-5/6">Cadastrar áreas de conhecimento</span>
+    </Link>,
+    <Link className="w-full flex" href="/admin/register/institutions">
+      <Plus className="w-1/6" />
+      <span className="w-5/6">Cadastrar instituições</span>
     </Link>,
   ];
   return menuItems;
