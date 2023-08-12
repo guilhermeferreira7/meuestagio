f571b24541e7c65507750bc7b4d2a087219acee2
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-08-12 14:46:00 -0300

    feat: add styles on vacancy page

--- client/src/pages/student/vacancy/[id].page.tsx
+++ client/src/pages/student/vacancy/[id].page.tsx
@@ -10,7 +10,7 @@ import { Vacancy } from "../../../utils/types/vacancy";
 export default function Vacancy() {
   const router = useRouter();
   const { id } = router.query;
-  const [vacancy, setVacancy] = useState<Vacancy>({} as Vacancy);
+  const [vacancy, setVacancy] = useState<any>({});
 
   useEffect(() => {
     api
@@ -24,14 +24,65 @@ export default function Vacancy() {
   }, []);
 
   return (
-    <div>
-      <h2>{vacancy.title}</h2>
-      <h2>{vacancy.description}</h2>
-      <h2>{vacancy.company?.name}</h2>
-      <h2>{vacancy.area?.title}</h2>
+    <>
+      <div className="w-full p-2">
+        <div className="card card-bordered p-3 w-full">
+          <div className="flex justify-between">
+            <h2 className="text-2xl font-bold py-3">
+              {vacancy.title} - {vacancy.remote ? "Remoto" : "Presencial"} -
+              Código da vaga: {vacancy.id}
+            </h2>
+
+            <button className="btn btn-sm h-12 btn-primary text-sm lg:text-xl m-2 normal-case">
+              Quero me candidatar
+            </button>
+          </div>
+          <div className="lg:flex flex-row">
+            <div className="lg:w-1/3 text-xl">
+              <h2>Área: {vacancy.area?.title}</h2>
+              <h2>Empresa: {vacancy.company?.name}</h2>
+              <h2>
+                Salário:{" "}
+                {vacancy.salary ? (
+                  <span className="font-semibold">R$ {vacancy.salary},00</span>
+                ) : (
+                  <span>Não informado</span>
+                )}
+              </h2>
+              <h2>Cidade: {vacancy.city?.name}</h2>
+
+              <h2>
+                {vacancy.keywords
+                  ?.split(", ")
+                  .map((keyword: any, index: any) => (
+                    <span
+                      key={index}
+                      className="font-semibold text-primary inline-block mr-4 underline"
+                    >
+                      {keyword}{" "}
+                    </span>
+                  ))}
+              </h2>
+            </div>
+            <div className="divider divider-horizontal"></div>
+
+            <div className="lg:w-2/3">
+              <h2 className="text-xl vacancyDescription">
+                Sobre a vaga:
+                <div>
+                  <div
+                    className="text-xl"
+                    dangerouslySetInnerHTML={{ __html: vacancy.description }}
+                  />
+                </div>
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <ToastContainer />
-    </div>
+    </>
   );
 }
 
