8a70e385a38ef3a6c965cff61485825bc84e119c
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-08-22 13:39:41 -0300

    refactor: Resume entity

--- api/src/modules/resumes/entities/resume.entity.ts
+++ api/src/modules/resumes/entities/resume.entity.ts
@@ -1,1 +1,42 @@
-export class Resume {}
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Student } from '../../users/students/entities/student.entity';
+import { Experience } from './experiences.entity';
+import { Education } from './education.entity';
+
+@Entity()
+export class Resume {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  studentId: number;
+
+  @ManyToOne(() => Student)
+  student: Student;
+
+  @Column()
+  title: string;
+
+  @Column()
+  about: string;
+
+  @Column()
+  skills: string;
+
+  @Column()
+  languages: string;
+
+  @OneToMany(() => Education, (education) => education.resume)
+  educations: Education[];
+
+  @OneToMany(() => Experience, (experience) => experience.resume, {
+    nullable: true,
+  })
+  experiences: Experience[];
+}
