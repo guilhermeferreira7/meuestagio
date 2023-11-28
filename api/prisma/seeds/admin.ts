73f390c634b560a16d3d52ea4836929864acfb6d
guilhermeferreira7 <guilhermefribas@outlook.com>
2023-11-27 22:44:13 -0300

    chore: update .env

--- api/prisma/seeds/admin.ts
+++ api/prisma/seeds/admin.ts
@@ -3,6 +3,6 @@ import bcryptService from '../../src/utils/bcriptUtils';
 console.log('Criando usuário administrador...');
 export const admin = {
   email: process.env.ADMIN_EMAIL,
-  name: process.env.ADMIN_NAME,
+  name: 'Administrador',
   password: bcryptService.hashSync(process.env.ADMIN_PASSWORD),
 };
